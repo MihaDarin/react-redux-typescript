ea955662d895f2b3697075dd63b78169790fec2c
Mike <mihaildaryin@gmail.com>
2022-08-09 16:50:58 +0300

    изученая работа с массивами и рефакторинг элементов редьюсера

--- src/store/CustomerReducer.js
+++ src/store/CustomerReducer.js
@@ -2,16 +2,26 @@ const defaultState = {
   customers: [],
 };
 
+const ADD_CUSTOMER = "ADD_CUSTOMER";
+const DELETE_CUSTOMER = "DELETE_CUSTOMER";
+
 export const CustomerReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_CUSTOMER": {
-      return { ...state, customers: state.customers + action.payload };
+    case ADD_CUSTOMER: {
+      return { ...state, customers: [...state.customers, action.payload] };
     }
-    case "DELETE_CUSTOMERS": {
-      return { ...state, customers: state.customers - action.payload };
+    case DELETE_CUSTOMER: {
+      return {
+        ...state,
+        customers: state.customers.filter(
+          (customer) => customer.id !== action.payload
+        ),
+      };
     }
     default: {
       return state;
     }
   }
 };
+export const addCustomerAction = (payload) => ({ type: ADD_CUSTOMER, payload });
+export const deleteCustomerAction = (payload) => ({ type: DELETE_CUSTOMER, payload });
