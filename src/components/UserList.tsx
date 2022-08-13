59fe5b08d1b7fa9d03ba98fb279efc2b6b84ed42
Mike <mihaildaryin@gmail.com>
2022-08-13 10:22:56 +0300

    добавлен хук useTypedSelector для работы с типами и состоянием

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const UserList: React.FC = () => {
-  const state = useSelector((state) => state);
-  console.log(state)
+  const {users, loading, error} = useTypedSelector((state) => state.user);
   return <div>UserList</div>;
 };
 
