e01bbcdf5af585213b4d1ada12c76412ca5ae9c4
Mike <mihaildaryin@gmail.com>
2022-08-18 23:46:43 +0300

    добавлена типизация для action

--- src/store/reducers/UserReducer.ts
+++ src/store/reducers/UserReducer.ts
@@ -1,8 +1,38 @@
 import React from "react";
 
-
-export const UserReducer = () =>{
-    return(
-        
-    )
+interface UserState {
+  users: any[];
+  loading: boolean;
+  error: null | string;
 }
+
+interface UserAction {
+  type: string;
+  payload?: any;
+}
+
+const initialState: UserState = {
+  users: [],
+  loading: false,
+  error: null,
+};
+
+const FETCH_USERS = "FETCH_USERS";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_ERROR = "FETCH_USERS_ERROR";
+
+export const UserReducer = (
+  state = initialState,
+  action: UserAction
+): UserState => {
+  switch (action.type) {
+    case FETCH_USERS:
+      return { loading: true, error: null, users: [] };
+    case FETCH_USERS_SUCCESS:
+      return { loading: false, error: null, users: action.payload };
+    case FETCH_USERS_ERROR:
+      return { loading: false, error: action.payload, users: [] };
+    default:
+      return state;
+  }
+};
