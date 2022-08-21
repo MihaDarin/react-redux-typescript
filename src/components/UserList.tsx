40b191f17a7710117aa8f6433d8c0b478a9366ea
Mike <mihaildaryin@gmail.com>
2022-08-21 22:02:30 +0300

    добавлен отдельный файл для acion-creators, и логика отрисовки массива пользователей

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { fetchUsers } from "../store/action-creators/user";
 
 export const UserList: FC = () => {
   const { users, error, loading } = useTypedSelector((state) => state.user);
-  return <div>UserList</div>;
+  const dispatch: Dispatch<any> = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (loading) {
+    return <h1>Идёт загрузка</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+  return (
+    <div>
+      {users.map((user) => {
+        return <div key={user.id}>{user.name} </div>;
+      })}
+    </div>
+  );
 };
