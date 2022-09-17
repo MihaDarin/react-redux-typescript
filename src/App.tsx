6e0436bd250c7fba4e436a6b0b4236a124588f09
Mike <mihaildaryin@gmail.com>
2022-09-17 19:12:09 +0300

    реализован базовый функционал для отображения списка пользователей

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { useAppDispatch } from "./hooks/redux";
+import { useAppDispatch, useAppSelector } from "./hooks/redux";
+import { fetchUsers } from "./store/reducers/ActionCreators";
 
 function App() {
-  
   const dispatch = useAppDispatch();
+  const { users } = useAppSelector((state) => state.userReducer);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
   return (
     <div>
-     
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            {user.id}. {user.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -17,7 +17,17 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-   
+    usersFetching(state) {
+      state.isLoading = true;
+    },
+    usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
+      state.isLoading = false;
+      state.users = action.payload;
+    },
+    usersFetchingError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
